f32c5950df4b34d925ef7afbaa3bd26dd4177df4
SatoshiOhya <conatus.11099@gmail.com>
2020-09-07 17:57:54 +0900

    add:複数のモジュールを使用することを想定してjoin Notificationを使えるようにした。内部でアドレスを保持する

--- xbee.hpp
+++ xbee.hpp
@@ -10,16 +10,33 @@
 
 #include <stdint.h>
 #include <array>
-
-#include "usart.h"
-
-
+#include <map>
+#include <bitset>
 
 template<unsigned Size = 30>
 class xbee_c{
 public:
 	xbee_c(){
 		buf[0] = 0x7E;
+	}
+
+	uint64_t setJoinNotification(uint8_t *buf, uint8_t length){
+		uint16_t tmp16bitAddr = *(buf + 15)<<8 | *(buf + 16);
+		std::bitset<sizeof(uint64_t)*8> tmp64bitAddr;
+		if(addrList.find(tmp16bitAddr) == addrList.end()){
+			for(uint8_t n=0; n<8; n++){
+				tmp64bitAddr <<= 8;
+				tmp64bitAddr |= *(buf + 17 + n);
+			}
+			addrList[tmp16bitAddr] = tmp64bitAddr.to_ullong();
+			addrtListSize += 1;
+			return tmp64bitAddr.to_ullong();
+		}
+		return 0;
+	}
+
+	uint8_t getAddrListSize(){
+		return addrList.size();
 	}
 
 	template <typename T> void assemblyTransmitPacket(const T& data){
@@ -69,12 +86,20 @@ public:
 		return this->bufSize;
 	}
 
-	bool isSuccessTransmitStatus(std::array<uint8_t, 11> transmitStatus){
-		return buf[8] == 0x00;
-	}
+	template <typename T = bool> T isSuccessTransmitStatus(uint8_t *transmitStatus){
+		bool tmpIsSuccess = *(transmitStatus + 8) == 0x00;
+		if(sizeof(T) == sizeof(bool)){
+			return tmpIsSuccess;
+		}else if(sizeof(T) == sizeof(uint64_t)){
+			if(tmpIsSuccess == true){
+				if(addrList.find(*(transmitStatus + 5) << 8 | *(transmitStatus + 6)) != addrList.end()){
+					return addrList[*(transmitStatus + 5) << 8 | *(transmitStatus + 6)];
+				}
+			}
+		}else{
 
-	bool isSuccessTransmitStatus(uint8_t *transmitStatus){
-		return *(transmitStatus + 8) == 0x00;
+		}
+		return 0;
 	}
 
 
@@ -94,10 +119,17 @@ public:
 		return 11;
 	}
 
+	constexpr uint8_t getNodeIdentificationIndicatorPacketSize(){
+		return 36;
+	}
+
 private:
-	UART_HandleTypeDef *huart;
 	std::array<uint8_t, Size> buf={};
 	uint8_t bufSize;
+	std::map<uint16_t, uint64_t> addrList;
+	uint8_t addrtListSize = 0;
+
+
 	void checkSum(uint8_t length){
 		uint32_t sum=0;
 		for(uint8_t n=0; n<length-4; n++){
@@ -105,8 +137,6 @@ private:
 		}
 		 buf[length-1] = 0xFF - (sum & 0xFF);
 	}
-
-
 };
 
 #endif /* INC_XBEE_HPP_ */
